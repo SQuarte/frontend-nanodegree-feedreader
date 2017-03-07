d1d79b4d53a2fd23a8a10602d7493acfbda273a3
Egor Khober <altagoga@gmail.com>
2017-03-07 18:33:37 +0300

    some fixes

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,5 +1,5 @@
-"use strict"
 $(function() {
+"use strict";
 
 
     beforeEach(function() {
@@ -7,12 +7,12 @@ $(function() {
             toHaveClass: function() {
                 return {
                     compare: function(actual, className) {
-                        return { pass: $(actual).hasClass(className) }
+                        return { pass: $(actual).hasClass(className) };
                     }
-                }
+                };
             }
-        })
-    })
+        });
+    });
 
     describe('RSS Feeds', function() {
 
@@ -26,10 +26,10 @@ $(function() {
             function testFeedUrlIsDefined(feed) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toEqual(0);
-            };
+            }
 
             allFeeds.forEach(function(feed) {
-                testFeedUrlIsDefined(feed)
+                testFeedUrlIsDefined(feed);
             });
         });
 
@@ -38,7 +38,7 @@ $(function() {
             function testFeedNameIsDefined(feed) {
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toEqual(0);
-            };
+            }
 
             allFeeds.forEach(function(feed) {
                 testFeedNameIsDefined(feed);
@@ -64,15 +64,12 @@ $(function() {
 
     describe('Initial Entries', function() {
         beforeEach(function(done) {
-            loadFeed(0, function() {
-                done();
-            })
+            loadFeed(0,done);
         });
         it('is loaded and completed their work', function(done) {
-            expect($('.feed').length).toBeGreaterThan(0);
-            expect($('.entry').length).toBeGreaterThan(0);
+            expect($('.feed .entry').length).toBeGreaterThan(0);
             done();
-        })
+        });
     });
 
     describe('New Feed Selecction', function() {
@@ -82,14 +79,14 @@ $(function() {
                 oldFeedHtml = $('.feed').html();
                 loadFeed(1, function() {
                     done();
-                })
-            })
+                });
+            });
         });
 
         it('loaded and changes', function(done) {
             expect($('.feed').html()).not.toBe(oldFeedHtml);
             done();
-        })
+        });
     });
 
 }());
